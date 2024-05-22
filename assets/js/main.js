857883af1805a4c461608319b7ca6ddf87102e6f
Fexrenise <bayramovafexrenise@gmail.com>
2024-05-22 10:10:54 +0400

    home responsive finish

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+// document.addEventListener("DOMContentLoaded", function() {
+//     const sliderWrapper = document.querySelector('.sponsorOne');
+//     const slides = document.querySelectorAll('.sponsorOne img');
+//     const totalSlides = slides.length;
+//     let currentIndex = 0;
+  
+//     function showNextSlide() {
+//       currentIndex++;
+//       console.log(currentIndex );
+//       if (currentIndex > totalSlides - 4) { // Loop back to the start
+//         currentIndex = 0;
+//       }
+//       slides.style.transform = `translateX(-${currentIndex * 25}%)`;
+//     }
+  
+//     setInterval(showNextSlide, 1000);
+//   });
